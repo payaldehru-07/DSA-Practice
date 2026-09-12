761acfe5e7ef41352e09784d6c2a1b6ca73ecbf1
payaldehru-2007 <payaldehru@gmail.com>
2026-09-12 16:42:29 +0530

    Add array basics

---
+++ 1_basics/01_ThingsToKnow/array.py
@@ -0,0 +1,13 @@
+##Take 5 integers as input from the user and store them in an array (list).Then print:
+##The complete array. The first element. The last element. The element at index 2.
+
+arr1 = int(input("Enter the number: "))
+arr2 = int(input("Enter the number: "))
+arr3 = int(input("Enter the number: "))
+arr4 = int(input("Enter the number: "))
+arr5 = int(input("Enter the number: "))
+arr = [arr1, arr2, arr3, arr4, arr5]
+print(arr)
+print(arr[0])
+print(arr[4])
+print(arr[2])
