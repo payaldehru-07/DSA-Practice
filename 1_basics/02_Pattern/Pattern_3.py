e128535279f60d6c5e42824b53832e40cc23163f
payaldehru-2007 <payaldehru@gmail.com>
2026-09-13 16:44:49 +0530

    Add pattern 3

---
+++ 1_basics/02_Pattern/Pattern_3.py
@@ -0,0 +1,14 @@
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+'''
+1
+12
+123
+1234
+12345
+'''
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(j+1 , end = "")
+    print()
